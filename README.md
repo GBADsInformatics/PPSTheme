979ae9311c6eeefb0e143e0c895651175e59cefe
Dennis, Gabriel (A&F, St. Lucia) <Gabriel.Dennis@csiro.au>
2021-08-13 10:42:54 +1000

    Added Livestock VOP data cleaning script.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # PPSTheme
 
 A space for code made by PPS modellers and analysts in the Global Burden of Animal Diseases (GBADs). For each of your models please follow the best practices set by GBADs Informatics, as articulated in the Data Governance Handbook.
+
+
+## Data Governance Handbook
+http://www.gbadske.org/Documentation/DataGovernanceHandbook/intro.html
+
+## AGROVOC format
+http://www.fao.org/agrovoc/format
+
+
+## Directory Structure
+
--- src/20210811_faoLivestockValueOfProductionDataCleaning.R
+++ src/20210811_faoLivestockValueOfProductionDataCleaning.R
@@ -2,7 +2,7 @@
 # Creator: Gabriel Dennis 
 # GitHub: denn173
 # Email: gabriel.dennis@csiro.au
-# Date last edited: 20210811
+# Date last edited: 20210813
 # 
 # This program contains code which reads in FASTOAT data from the 
 # QV table (Value of Agricultural Production) 
@@ -22,35 +22,58 @@ source('src/FAOSTAT_helper_functions.R')
 
 ## 1 - Subset data and select columns
 
+# Temporary directory path and temporary file names
 tmp_dir <- file.path('data', 'temp')
 tmp_file <- file.path(tmp_dir,
                       'Value_of_Production_E_All_Data_(Normalized).zip')
+tmp_parquet <- file.path(tmp_dir, paste0(format(Sys.Date(),'%Y%m%d'), 
+                         '_Value_of_Production_E_All_Data.parquet'))
+
 
 # Read in Data using FAOSTAT package
 # store in data/temp and remove afterwards
-if (file.exists(tmp_file)) {
-  livestock_df <- readr::read_csv(tmp_file, 
-                                  col_select = c("Area Code", "Item",
-                                                 "Element Code", "Year", 
-                                                  "Unit", "Value"))
+if (file.exists(tmp_parquet)) {
+  livestock_df <- arrow::read_parquet(tmp_parquet)
 } else {
+  # Get the bulk normalized zip
   livestock_df <- FAOSTAT::get_faostat_bulk(code = 'QV', tmp_dir)
+  
+  if (!file.exists(tmp_file)) {
+    errorCondition(paste0("File was stored with in different file name, "))
+    }
+  
+  # Save to parquet file
+  arrow::write_parquet(livestock_df,tmp_parquet)
 }
 
 # Subset the data for year > 1991
 # Subset the data for regex of item "meat|milk|eggs"
 livestock_df <- livestock_df %>% 
   rename_with(tolower) %>% 
-  rename_with(~ str_replace_all(.x, " ", "_")) %>%
-  filter(year > 1991, 
-         str_detect(item, "Meat|Milk|Eggs"), 
-         element_code ==  '58')  %>% 
-  select(area_code, item, year, unit, value) 
+  rename_with(
+    ~ str_replace_all(.x, " ", "_")
+    ) %>%
+  filter(
+    year > 1991, 
+    str_detect(item, "Meat|Milk|Eggs"), 
+    element_code ==  '58'
+    )  %>% 
+  select(
+    area_code, item, year, unit, value
+    ) %>% 
+  mutate(
+    iso3c = countrycode::countrycode(area_code,'fao', 'iso3c')
+    ) %>% 
+  drop_na() %>% 
+  separate(
+    item, c('item', 'animal'), sep = ','
+    ) %>% 
+  filter(
+    !str_detect(animal, 'Total')
+    ) %>% 
+  mutate(animal = trimws(str_remove(animal, 'whole fresh'))) 
 
-## 3 - Code Data for ISO3C regions and drop unmatched regions
-livestock_df$iso3c <- countrycode::countrycode(livestock_df$area_code,
-                                               'fao', 'iso3c')
-livestock_df <- drop_na(livestock_df)
+
 
 
 ## 4 - Split item into animal, item
@@ -58,9 +81,14 @@ livestock_df <- drop_na(livestock_df)
 # Todo this - remove whole fresh, total from item
 livestock_df <- livestock_df %>% 
   separate(
-    item, c('item', 'animal'), sep = ',') %>% 
-  filter(!str_detect(animal, 'Total')) %>% 
-  mutate(animal = trimws(str_remove(animal, 'whole fresh'))) 
+    item, c('item', 'animal'), sep = ','
+    ) %>% 
+  filter(
+    !str_detect(animal, 'Total')
+    ) %>% 
+  mutate(
+    animal = trimws(str_remove(animal, 'whole fresh'))
+    ) 
 
 ## 4 - Save to file
 output_file <- file.path('data', 'output', paste0(format(Sys.Date(),'%Y%m%d'), 
@@ -72,7 +100,7 @@ readr::write_csv(livestock_df, file = paste0(output_file, 'csv'))
 ## 5 - Clean up
 if (file.exists(tmp_file) && 
     file.exists(paste0(output_file, 'csv'))) {
-  file.remove(tmp_file)
+  file.remove(c(tmp_file, tmp_parquet))
 }
 
 

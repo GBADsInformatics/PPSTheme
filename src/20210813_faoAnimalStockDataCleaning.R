95b0b460932e9cd27f4bed7c3e8325049f45b270
Dennis, Gabriel (A&F, St. Lucia) <Gabriel.Dennis@csiro.au>
2021-08-25 12:38:33 +1000

    Updated livestock items to drop

--- src/20210813_faoAnimalStockDataCleaning.R
+++ src/20210813_faoAnimalStockDataCleaning.R
@@ -2,14 +2,14 @@
 # Creator: Gabriel Dennis 
 # GitHub: @denn173
 # Email: gabriel.dennis@csiro.au
-# Date last edited: 20210813
+# Date last edited: 20210825
 # 
 # 
 # This script contains code which reads in FASTOAT data from the 
 # QCL table (Crops and livestock products) 
 # currently located at http://www.fao.org/faostat/en/#data/QCL
 # cleans the data, coding the countries to ISO3C regions 
-# saves the output since to the file
+# saves the output animal stock values to the file
 # data/output/{YYYYMMDD}_FAOSTAT_Livestock_Stocks.csv
 ####################################
 
@@ -41,24 +41,29 @@ if (file.exists(tmp_parquet)) {
   # Get the bulk normalized zip
   livestock_df <- FAOSTAT::get_faostat_bulk(code = fao_code, tmp_dir)
   
+  # column names we will use 
+  col_vars <- c('area_code', 'item', 'element', 'year', 'unit', 'value')
+  if (length(setdiff(col_vars, names(livestock_df))) > 0) {
+    stop(paste0('FAOSTAT raw data has incorrect column names: ',
+                names(livestock_df)))
+  }
   # Check the file was stored under the correct name
   if (!file.exists(tmp_file)) {
-    errorCondition(paste0("File was stored with in different file name, "))
+    stop(paste0("File was stored with incorrect  file name."))
   }
   # Save to parquet file
   arrow::write_parquet(livestock_df,tmp_parquet)
 }
 
 
-## 2 - Data Cleaning pipeline
-##  livestock_df has the columns
-## area_code area item_code item element_code element year_code year unit value flag
+## 2 - Initial data cleaning pipeline ------------------------------#
+
 livestock_df <- livestock_df %>% 
   select(
     area_code, item, element, year, unit, value
   ) %>% 
   filter(
-    element == 'Stocks'
+    element == 'Stocks', item != 'Beehives'  # Drop Beehives as this won't be necessary
   ) %>% 
   mutate(
     iso3c = countrycode::countrycode(area_code, 'fao', 'iso3c')
@@ -71,81 +76,113 @@ livestock_df <- livestock_df %>%
     -unit, -value, -element, -area_code
   )  
 
-## 3 - Item categories which contain multiple animals
-## Cattle and Buffaloes
-## Sheep and Goats  
-## 
-## For these Items, for the moment we will only keep the double
-## counted entry if the country does not report single entries 
-## for both of the items. 
-## 
-## This can be seen in the commented lines below
-## 
-# livestock_df %>% 
-#   filter(
-#     str_detect(item, "Cattle|Buffaloes")
-#   ) %>% 
-#   pivot_wider(
-#     names_from = item, values_from = head
-#   ) %>% 
-#   replace_na(
-#     list(Buffaloes = 0)
-#   ) %>% 
-#   mutate(
-#     diff = abs(Cattle + Buffaloes - `Cattle and Buffaloes`)
-#   )  %>% 
-#   summary()
-# 
-# livestock_df %>% 
-#   filter(
-#     str_detect(item, "Sheep|Goats")
-#   ) %>% 
-#   pivot_wider(
-#     names_from = item, values_from = head
-#   ) %>% 
-#   replace_na(
-#     list(Sheep = 0, Goats = 0)
-#   ) %>% 
-#   mutate(
-#     diff = abs(Sheep + Goats - `Sheep and Goats`)
-#   )  %>% 
-#   summary()
+if (nrow(livestock_df) == 0) {
+  stop('Incorrect filtering operation')
+}
 
-# This can be achieved by keeping the double categories if
-# one of the sub categories does not equal it. 
 
 livestock_df <- livestock_df %>% 
   mutate(
-    item = tolower(str_replace_all(item, ' ', '_'))
+    item = tolower(
+      str_replace_all(str_remove_all(item, ','), ' ', '_')
+      )
   ) %>% 
   pivot_wider(
     names_from = item, values_from = head
   ) 
 
-# Add columns to code when to keep each item
-# NA codes to FALSE
-livestock_df$keep_sheep_goats<- purrr::map_lgl(1:nrow(livestock_df), 
-                   function(i) {
-                     livestock_df$sheep[i] == livestock_df$sheep_and_goats[i] ||
-                     livestock_df$goats[i] == livestock_df$sheep_and_goats[i] 
-                   })
+## 4 - Double Counting Categories ------------------------------#
+## 
+## Some categories contain entries from multiple livestock types
+## Cattle and Buffaloes - cattle and buffaloes
+## Sheep and Goats   - sheep and goats
+## poultry, birds - All poultry birds including chicken and gees
+## 
+## These are included to make long term comparisons easier for countries 
+## which do not report all animal types. 
 
-livestock_df$keep_buffaloes_cattle <- purrr::map_lgl(1:nrow(livestock_df), 
-                   function(i) {
-                     livestock_df$buffaloes[i] == livestock_df$cattle_and_buffaloes[i] ||
-                       livestock_df$cattle[i] == livestock_df$cattle_and_buffaloes[i] 
-                   })
+### CATTLE AND BUFFALOES 
+# # Check the only countries which report the multiple category
+# 
+# Cattle is only NA when cattle and buffaloes are NA
+# all(is.na(livestock_df$cattle) == is.na(livestock_df$cattle_and_buffaloes))
+# 
+# # Buffaloes are always equal to cattle_and_buffaloes - cattle
+# livestock_df %>%
+#   select(iso3c, year, cattle, buffaloes, cattle_and_buffaloes) %>%
+#   filter(!is.na(buffaloes)) %>%
+#   mutate(
+#     buffaloes_imp = (cattle_and_buffaloes - cattle) == buffaloes
+#   ) %>%
+#   count(buffaloes_imp)  # All True
 
-# Set index value to drop
-livestock_df$sheep_and_goats[livestock_df$keep_sheep_goats != TRUE] <- NA
-livestock_df$cattle_and_buffaloes[livestock_df$keep_buffaloes_cattle != TRUE] <- NA
+## DECISION: DROP Cattle and Buffaloes 
+
+### Sheep and Goats 
+
+# > sum(is.na(livestock_df$sheep) != is.na(livestock_df$sheep_and_goats))
+# [1] 598
+# > sum((is.na(livestock_df$goats)  & is.na(livestock_df$goats)) != is.na(livestock_df$sheep_and_goats))
+# [1] 289
+# > sum((is.na(livestock_df$goats)  & is.na(livestock_df$sheep)) != is.na(livestock_df$sheep_and_goats))
+# [1] 0
+
+# No countries only report sheep and goats
+#  - Check that it is always equal to the difference
+# livestock_df %>%
+#   select(iso3c, year, sheep, goats, sheep_and_goats) %>%
+#   filter(!is.na(sheep)) %>%
+#   mutate(
+#     sheep_imp = if_else(is.na(goats), 
+#                         sheep_and_goats == sheep, 
+#                         sheep_and_goats  - goats == sheep),
+#   ) %>%
+#   count(sheep_imp) # All true
+# 
+# livestock_df %>%
+#   select(iso3c, year, sheep, goats, sheep_and_goats) %>%
+#   filter(!is.na(goats)) %>%
+#   mutate(
+#     goat_imp = if_else(is.na(sheep),
+#                         sheep_and_goats == goats,
+#                         sheep_and_goats  - sheep == goats),
+#   ) %>%
+#   count(goat_imp) # All true
+## DECISION: DROP Sheep and Goats
+### 
+
+### Poultry Birds
+
+# Check if it is the sum of Chickens, Turkeys, geese_and_guinea_fowls
+# table(rowSums(select(livestock_df, 
+#                      chickens, 
+#                      geese_and_guinea_fowls,
+#                      ducks,
+#                      turkeys), na.rm = TRUE) == livestock_df$poultry_birds)
+# FALSE  TRUE 
+# 393 10196 
+
+# Check where this is not the case
+# livestock_df %>% 
+#   select(iso3c, year, chickens, geese_and_guinea_fowls, ducks,  turkeys, poultry_birds) %>% 
+#   rowwise() %>% 
+#   mutate(
+#     diff = poultry_birds - sum(c_across(chickens:turkeys),  na.rm = TRUE) ,
+#     diff_pct = round((diff/poultry_birds) * 100)
+#   ) %>% 
+#   filter(diff_pct > 10) %>% 
+#   count(iso3c)
+
+# Only has a greater than 10 % difference in Hong Kong, Cyprus,Syria and Egypt
+# Non of these differences occur after 1990
+
+# DECISION: Drop Poultry Birds
 
 
 # Drop values and tidy data
 livestock_df <- livestock_df %>% 
   select(
-    -keep_sheep_goats, 
-    -keep_buffaloes_cattle
+    -cattle_and_buffaloes, -sheep_and_goats, -poultry_birds
   ) %>% 
   pivot_longer(
     !c("year", "iso3c"),
@@ -154,7 +191,11 @@ livestock_df <- livestock_df %>%
     values_drop_na = TRUE
    )  
   
-## 4 - Save to file
+
+
+
+## 5 - Save to file ------------------------------#
+
 output_file <- file.path(
   'data', 'output',
   paste0(format(Sys.Date(),'%Y%m%d'), 
@@ -173,7 +214,7 @@ file.remove(file.path(output_dir, output_files[remove_files]))
 arrow::write_parquet(livestock_df,paste0(output_file, 'parquet') )
 readr::write_csv(livestock_df, file = paste0(output_file, 'csv'))
 
-## 5 - Clean up
+## 6 - Clean up
 ## Only remove the temporary zip file for now.
 if (file.exists(tmp_file) && 
     file.exists(paste0(output_file, 'csv'))) {

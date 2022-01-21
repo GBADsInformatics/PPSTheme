edaa2da363870cf63db28ba7747a3dcd0f4c9339
Kassy Raymond <44474665+kassyray@users.noreply.github.com>
2022-01-21 13:10:46 -0500

    Updated biomass numbers and code
    Updated biomassCalc code to support estimates from the OIE. Updated data files to reflect conversion from grams to kg for rabbits and turkey

--- src/BiomassCalc.py
+++ src/BiomassCalc.py
@@ -10,19 +10,33 @@ from cleanFaoConversion import GBADsAPI as GBADsAPI
 if __name__ == '__main__':
 
 	# Read in all data 
-	if len(sys.argv) != 2: 
+	if len(sys.argv) != 4: 
 		sys.exit('Please provide original live weight table as a system argument.')
+	elif sys.argv[2] != 'oie' and sys.argv[2] != 'faostat':
+		sys.exit('Please provide valid data source: faostat or oie')
 	else: 
 		file = sys.argv[1]
-		live_weight = pd.read_csv(file, encoding='utf8')
+		data_source = sys.argv[2]
+		outpath = sys.argv[3]
 
-	#FIXME put this as a command line argument where can only accept options supported by API
-	data_source = 'faostat' 
+	try: 
+		live_weight = pd.read_csv(file, encoding='utf8')
+	except: 
+		sys.exit('Could not read file. Ensure that the file exists and correct file path is provided.')
+
+
+	# FIXME: Would be best if the years were not hard coded... 
+	if data_source == 'faostat': 
+		start_year = 1961
+		end_year = 2017
+	elif data_source == 'oie': 
+		start_year = 2005
+		end_year = 2017
 
 	dfs = []
 
 	# produce all years of data
-	for year in range(1961, 2018):
+	for year in range(start_year, end_year+1):
 
 		data = GBADsAPI.make_custom_call(str(year), data_source)
 				
@@ -46,7 +60,6 @@ if __name__ == '__main__':
 
 	# Create outfile name 
 	now = datetime.datetime.now()
-	outfile = "%s_biomass_liveWeight_%s.csv" % (now.strftime("%Y%m%d"), data_source)
+	outfile = "%s/%s_biomass_liveWeight_%s.csv" % (outpath, now.strftime("%Y%m%d"), data_source)
+
 	df_full.to_csv(outfile, index = False)
-
-
